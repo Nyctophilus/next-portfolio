66041369b453e52573980a01fafbeb5db177b167
Medo <mohammed.yuossry@gmail.com>
2024-01-07 07:43:29 +0200

    add: stars bg

---
+++ src/app/components/StarsBg/StarsBg.jsx
@@ -0,0 +1,10 @@
+import styles from "./StarsBg.module.css";
+
+const StarsBg = () => {
+  return (
+    <div className="w-full fixed inset-0">
+      <div className={styles.StarsBg} />
+    </div>
+  );
+};
+export default StarsBg;
--- src/app/page.js
+++ src/app/page.js
@@ -1,45 +1,35 @@
 "use client";
 import dynamic from "next/dynamic";
-const HeroSection = dynamic(() => import("./components/HeroSection"), {
-  ssr: false,
-});
-const ProjectsSection = dynamic(
-  () => import("./components/Projects/ProjectsSection"),
-  {
-    ssr: false,
-  }
+const HeroSection = dynamic(() => import("./components/HeroSection"));
+const ProjectsSection = dynamic(() =>
+  import("./components/Projects/ProjectsSection")
 );
-const AboutSection = dynamic(() => import("./components/AboutSection"), {
-  ssr: false,
-});
+const AboutSection = dynamic(() => import("./components/AboutSection"));
 
-const AchievementsSection = dynamic(
-  () => import("./components/AchievementsSection"),
-  {
-    ssr: false,
-  }
+const AchievementsSection = dynamic(() =>
+  import("./components/AchievementsSection")
 );
-const EmailSection = dynamic(() => import("./components/EmailSection"), {
-  ssr: false,
-});
-const Footer = dynamic(() => import("./components/Footer"), {
-  ssr: false,
-});
+const EmailSection = dynamic(() => import("./components/EmailSection"));
+const Footer = dynamic(() => import("./components/Footer"));
 
 import Navbar from "./components/Navbar/Navbar";
+import StarsBg from "./components/StarsBg/StarsBg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <>
+      <StarsBg />
       <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
-        <AchievementsSection />
-        <AboutSection />
-        <ProjectsSection />
-        <EmailSection />
-      </div>
-      <Footer />
-    </main>
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+        <div className="container mt-24 mx-auto px-12 py-4">
+          <HeroSection />
+          <AchievementsSection />
+          <AboutSection />
+          <ProjectsSection />
+          <EmailSection />
+        </div>
+        <Footer />
+      </main>
+    </>
   );
 }
